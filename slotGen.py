550c79a5b0e93573c96e3df648b248815b8208ca
Theriley106 <christopherlambert106@gmail.com>
2018-02-10 15:53:33 -0500

    Added split at vowel to get every point that someone may stuter

--- slotGen.py
+++ slotGen.py
@@ -1,10 +1,33 @@
-def splitAtVowel(word):
-	wordPossibilities = []
-	# This simulates stuttering
-	vowels = list("aeiou")
-	for i, letter in enumerate(word):
-		if letter in vowels:
-			print(word[:i] + ' ' + word)
-			#print(word[:i])
+# Only says part of the word
+# Find time that alexa listens before going to response
+# Generate Stuttered words
 
-splitAtVowel("calculator")
+def indexOfNextVowel(partialWord, i):
+	for letter in partialWord:
+		if letter in list("aeiou"):
+			return partialWord.index(letter) + i
+
+def indexOfPreviousVowel(partialWord, i):
+	return
+
+def splitAtVowel(word):
+	# This splits words at every point in which the person may stutter
+	wordPossibilities = []
+	for i, letter in enumerate(list(word)):
+		if letter not in list("aeiou"):
+			if i+1 < len(word):
+				newWord = letter
+				i += 1
+				nextLetter = list(word)[i]
+				while nextLetter not in list("aeiou"):
+					newWord = newWord + nextLetter
+					i += 1
+					print i
+					if i < len(word):
+						nextLetter = word[i]
+					else:
+						break
+				wordPossibilities.append(newWord)
+	return wordPossibilities
+
+print splitAtVowel("catastrophic")
