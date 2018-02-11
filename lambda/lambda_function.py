47f36f9dc6f58951742243307ff00e77545f6296
Theriley106 <christopherlambert106@gmail.com>
2018-02-11 01:55:43 -0500

    added the num to word and proper sentence structure

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -27,6 +27,40 @@ def lambda_handler(event, context):
 	elif event["request"]["type"] == "IntentRequest":
 		return on_intent(event["request"], event["session"])
 
+def text2int(textnum, numwords={}):
+    if not numwords:
+      units = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+        "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen",
+      ]
+
+      tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+      scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+      numwords["and"] = (1, 0)
+      for idx, word in enumerate(units):    numwords[word] = (1, idx)
+      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+          raise Exception("Illegal word: " + word)
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
+
+def valToNum(wordVal):
+  word = wordVal.replace("word", "").lower().replace("thirty", "thirty ").replace("twenty", "twenty ")
+  return text2int(word)
+
 def counter_intent(event, context):
     session_attributes = event['session']['attributes']
     if "counter" in session_attributes:
@@ -47,6 +81,22 @@ def conversation(title, body, session_attributes):
     return build_response(speechlet,
                           session_attributes=session_attributes)
 
+def getAllSlots(intent):
+	dictValue = {}
+	sentence = ""
+	for slot in intent['intent']['slots'].keys():
+		try:
+			numVal = str(valToNum(intent['intent']['slots'][slot]['name']))
+			dictValue[numVal] = intent['intent']['slots'][slot]['value']
+		except Exception as exp:
+			pass
+	i = 0
+	for i in range(1,40):
+		try:
+			sentence = sentence + " " + dictValue[str(i)]
+		except:
+			pass
+	return sentence
 
 def on_intent(intent_request, session):
 	print str(session)
@@ -58,11 +108,11 @@ def on_intent(intent_request, session):
 		print session['attributes']['counter']
 		e = session['attributes']['counter']
 		count = e + 1
+		print getAllSlots(intent_request)
 		if count > 2:
 			return createResponse("End session", True, sessionCount=count)
 		else:
 			return createResponse("Repeat the following sentence. {}".format(random.choice(sentences)), False, sessionCount=count)
-
 	elif intent_name == 'aboutDev':
 		return alexaHelper.devInfo()
 	elif intent_name == "AMAZON.HelpIntent":
