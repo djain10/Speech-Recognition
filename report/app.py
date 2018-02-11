7a9158dbe3bbee4f46cbd8440af91dd0b259381a
Theriley106 <christopherlambert106@gmail.com>
2018-02-11 09:24:48 -0500

    Centered text in search box

--- report/app.py
+++ report/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response, jsonify
-app = Flask(__name__, static_url_path="", static_folder="static")
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template("index.html", DATABASE=xUtilities.genMakeIndex())
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.run()
